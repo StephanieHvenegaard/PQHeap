24866df9603a0ed1a229187b9ec7b220c44de508
Simon <sije817@student.sdu.dk>
2019-05-07 15:15:18 +0200

    project 3
    project 3 initial commit

---
+++ Encode/src/encode/Encode.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encode;
+
+/**
+ *
+ * @author Simon Nyborg Jensen sije817@student.sdu.dk
+ * @author Joakim Sjurman Larsen joala09@student.sdu.dk
+ * @author Stephanie Rømer Hvenegaard sthve16@student.sdu.dk
+ */
+public class Encode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
